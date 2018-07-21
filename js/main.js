96cebe04a4d9225e328707822a4b446792c90422
jorgehs91 <jorgehs_lol@hotmail.com>
2018-07-21 17:40:36 -0300

    Iniciando o curso

---
+++ js/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var Client = function Client(nome, email) {
+    _classCallCheck(this, Client);
+
+    this.nome = nome;
+    this.email = email;
+};
---
+++ src/app.js
@@ -0,0 +1,6 @@
+class Client {
+    constructor(nome,email){
+        this.nome = nome;
+        this.email = email;
+    }
+}
